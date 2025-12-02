53a93e23ee5bf0ca43f6cb7168e3e858d1976803
Mauricio <maau.cursos@gmail.com>
2025-12-01 22:46:36 -0600

    Listando las propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,28 @@
 <?php 
 
+// Importar la conexion 
+require '../includes/config/database.php'; 
+$db = conectarDB(); 
 
+// Escribir el Query 
+$query = "SELECT * FROM propiedades"; 
+
+
+// Consultar la BD
+$resultadoConsulta = mysqli_query($db, $query); 
+
+
+
+// Muestra mensjae condicional 
 $resultado = $_GET['resultado'] ?? null; 
+
+// Incluye un template
 require '../includes/funciones.php';
 
 incluirTemplates('header'); 
 ?> 
+
+
     <main class="contenedor">
         <h1>Administrador de Bienes Raices</h1>
         <?php if( intval( $resultado ) === 1):?>
@@ -26,17 +43,19 @@ incluirTemplates('header');
               </tr>              
             </thead>
 
-            <tbody>
+            <tbody> <!--. Mostar los resultados -->
+              <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)):?>
               <tr>
-                <td>1</td>
-                <td>Casa en la playa</td>
-                <td><img src="/imagenes/2d668568b7d8f65df77e12bf8cfd1b36.jpg" class="imagen-tabla"></td>
-                <td>$1200000</td>
+                <td> <?php echo $propiedad["id"]; ?> </td>
+                <td>  <?php echo $propiedad["titulo"]; ?> </td>
+                <td><img src="/imagenes/<?php echo $propiedad["imagen"]; ?> " class="imagen-tabla"></td>
+                <td> <?php echo $propiedad["precio"]; ?> </td>
                 <td>
                   <a href="#" class="boton-rojo-block">Eliminar</a>
                   <a href="#" class="boton-amarillo-block">Actualizar</a>
                 </td>
               </tr>
+              <?php endwhile; ?>
 
             </tbody>
 
@@ -44,6 +63,9 @@ incluirTemplates('header');
     </main>
 
   <?php
+    // Cerrar la conexion
+    mysqli_close($db);
+
     incluirTemplates('footer'); 
   ?>
 
