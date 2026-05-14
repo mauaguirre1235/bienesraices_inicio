8524f05f05970ee3cd0fca141b435254e3339ee5
Mauricio <maau.cursos@gmail.com>
2026-05-14 12:25:56 -0600

    Añadiendo validaciones a la clase de propiedad

--- classes/propiedad.php
+++ classes/propiedad.php
@@ -37,4 +37,48 @@ protected static $tabla = 'propiedades';
     $this->creado = date('Y/m/d');
     $this->vendedores_id = $args['vendedores_id'] ?? '';
   }
+
+
+    public function validar()
+  {
+
+    if (!$this->titulo) {
+      self::$errores[] = "Debes añadir un titulo";
+    }
+
+
+    if (!$this->precio) {
+      self::$errores[] = "El precio es obligatorio";
+    }
+
+    if (strlen($this->descripcion) < 50) {
+      self::$errores[] = "La descripcion es obligatoria y debe tener al menos 50 caracteres";
+    }
+
+    if (!$this->habitaciones) {
+      self::$errores[] = "El Numero de habitaciones es obligatorio";
+    }
+
+    if (!$this->wc) {
+      self::$errores[] = "El Numero de baños es obligatorio";
+    }
+
+    if (!$this->estacionamiento) {
+      self::$errores[] = "El Numero de lugar de Estacionamiento es obligatorio";
+    }
+
+    if (!$this->vendedores_id) {
+      self::$errores[] = "Elige un vendedor";
+    }
+
+    if (!$this->imagen) {
+      self::$errores[] = "La imagen es obligatoria";
+    }
+
+
+
+
+    return self::$errores;
+  }
+
 }
