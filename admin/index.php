33f225791acaf7f833b0061907c8be2807cc2f1f
Mauricio <maau.cursos@gmail.com>
2026-05-19 15:40:09 -0600

    creando el formulario para registrar vendedores

--- admin/index.php
+++ admin/index.php
@@ -60,6 +60,7 @@ incluirTemplates('header');
   <?php endif; ?>
 
   <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+  <a href="/admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo Vendedor</a>
   <h2>Propiedades</h2>
   <table class="propiedades">
     <thead>
