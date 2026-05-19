b5e82e3fa169d7b08063479b602b22a997bb0b02
Mauricio <maau.cursos@gmail.com>
2026-05-19 16:10:23 -0600

    Validacion del formulario

--- admin/Vendedores/crear.php
+++ admin/Vendedores/crear.php
@@ -16,7 +16,16 @@ $errores = Vendedor::getErrores();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+  // Crear una nueva instancia 
+  $vendedor = new Vendedor($_POST['vendedor']);
 
+  // validar que no haya campos vacios 
+  $errores= $vendedor->validar(); 
+
+  // No hay errores 
+  if(empty($errores)) { 
+    $vendedor->guardar();
+  }
 
 }
 
--- admin/index.php
+++ admin/index.php
@@ -52,11 +52,11 @@ incluirTemplates('header');
 <main class="contenedor">
   <h1>Administrador de Bienes Raices</h1>
   <?php if (intval($resultado) === 1): ?>
-    <p class="alerta exito">Anuncio Creado Correctamente</p>
+    <p class="alerta exito">Creado Correctamente</p>
   <?php elseif (intval($resultado) === 2): ?>
-    <p class="alerta exito">Anuncio Actualizado Correctamente</p>
+    <p class="alerta exito">Actualizado Correctamente</p>
   <?php elseif (intval($resultado) === 3): ?>
-    <p class="alerta exito">Anuncio Eliminado Correctamente</p>
+    <p class="alerta exito">Eliminado Correctamente</p>
   <?php endif; ?>
 
   <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
